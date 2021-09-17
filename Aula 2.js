690ae6a4e43d10bc052b78b743d3de22ca0cbefa
Allan Gabriel <euallangamer@gmail.com>
2021-09-16 22:23:38 -0300

    first commit

---
+++ Aula 2.js
@@ -0,0 +1,13 @@
+
+var alunosQuant = 12
+console.log("0") // Não sei se está certo, Porém foi a única forma que consigui de colocar "0 é ZERO"
+console.log(" é ZERO")
+for (let index = 1; index <=alunosQuant; index++){
+    console.log(index)
+    if(index % 2== 0){console.log(" é par")
+}else{console.log(" é impar")
+ }
+}
+console.log("FIM")
+
+// perdão o deslinhamento na hora de excutar o código professor, espero que não atrapalhe a visualização
